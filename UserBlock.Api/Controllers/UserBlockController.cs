1a74c3e122e746061393add0b30c09b22209301b
meetic-mniare <m.niare@meetic-corp.com>
2024-05-13 11:48:33 +0200

    Init

--- UserBlock.Api/Controllers/UserBlockController.cs
+++ UserBlock.Api/Controllers/UserBlockController.cs
@@ -7,12 +7,19 @@ namespace UserBlock.Api.Controllers;
  [Authorize]
 [ApiController]
 [Route("api/userBlock")]
-public class UserBlockController: ControllerBase
+public class UserBlockController: UserBlockControllerBase
 {
     [HttpPost("block")]
     public IActionResult BlockUser(UserInfo userInfo)
     {
-        return Ok(userInfo);
+        return Ok( new
+        {
+            UserInfo = userInfo, 
+            Blocked = true,
+            BlockedReason = "User Blocked",
+            BlockedDate = DateTime.Now,
+            blockedBy = CurrentUserId,
+        });
     }
     
 }
---
+++ UserBlock.Api/Controllers/UserBlockControllerBase.cs
@@ -0,0 +1,18 @@
+using System.Security.Claims;
+using Microsoft.AspNetCore.Mvc;
+
+namespace UserBlock.Api.Controllers;
+
+public class UserBlockControllerBase : ControllerBase
+{
+    protected Guid CurrentUserId
+    {
+        get
+        {
+            var userIdClaim = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+            return Guid.TryParse(userIdClaim, out var userId)
+                ? userId
+                : throw new ArgumentNullException("userId");
+        }
+    }
+}
